d1f8a9f40cac50de0d3ab2a2fba50ecdd5373467
somnus9527 <somnus9527@yeah.net>
2024-01-19 10:44:08 +0800

    优化nvim-cmp提示

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,18 +1,60 @@
 return {
   'hrsh7th/nvim-cmp',
-  event = 'InsertEnter',
+  event = { 'InsertEnter', 'CmdlineEnter' },
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
-    -- 'L3MON4D3/LuaSnip',
+    'L3MON4D3/LuaSnip',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
-    'David-Kunz/cmp-npm',
+    -- 'David-Kunz/cmp-npm',
     'hrsh7th/cmp-nvim-lua',
     'onsails/lspkind.nvim',
   },
   config = function()
-    -- local luasnip = require 'luasnip'
+    -- 自定义样式
+    vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#282C34", fg = "NONE" })
+    vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "#22252A" })
+
+    vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
+    vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
+    vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
+    vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
+
+    vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#EED8DA", bg = "#B5585F" })
+    vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
+    vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
+
+    vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#C3E88D", bg = "#9FBD73" })
+    vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
+    vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
+
+    vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
+    vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
+    vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
+
+    vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
+    vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
+    vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#EADFF0", bg = "#A377BF" })
+    vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#EADFF0", bg = "#A377BF" })
+    vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
+
+    vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
+    vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
+
+    vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
+    vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
+    vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
+
+    vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#DDE5F5", bg = "#6C8ED4" })
+    vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#DDE5F5", bg = "#6C8ED4" })
+    vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#DDE5F5", bg = "#6C8ED4" })
+
+    vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#D8EEEB", bg = "#58B5A8" })
+    vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#D8EEEB", bg = "#58B5A8" })
+    vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = "#D8EEEB", bg = "#58B5A8" })
+    -- 自定义样式结束
+    local luasnip = require 'luasnip'
     local lspkind = require 'lspkind'
     local icons = require 'configs.icons'
     local cmp = require 'cmp'
@@ -25,26 +67,66 @@ return {
       completion = {
         completeopt = 'menu,menuone,noinsert',
       },
-      -- snippet = {
-      --   expand = function(args)
-      --     luasnip.lsp_expand(args.body)
-      --   end,
-      -- },
-      sources = cmp.config.sources {
-        { name = 'nvim_lsp' },
-        -- { name = 'luasnip' },
-        { name = 'buffer' },
-        { name = 'path' },
-        { name = 'npm' },
-        { name = 'nvim_lua' },
-        { name = 'cmdline' },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      -- 这两个table的区别是，先在第一个table里面找，找不到再去第二个table找
+      sources = cmp.config.sources(
+        {
+          {name = 'nvim_lsp'},
+        },
+        {
+          {name = 'buffer'},
+          {name = 'path'}
+        }
+      ),
+      window = {
+        completion = {
+          winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
+          col_offset = -3,
+          side_padding = 0,
+        },
       },
       formatting = {
-        format = lspkind.cmp_format {
-          mode = 'symbol_text',
-          preset = 'codicons',
-          symbol_map = icons.kinds,
-        },
+        -- format = lspkind.cmp_format {
+        --   mode = 'symbol_text',
+        --   preset = 'codicons',
+        --   symbol_map = icons.kinds,
+        --   menu = {
+        --     buffer = '[Buffer]',
+        --     nvim_lsp = '[LSP]',
+        --     path = '[Path]',
+        --     nvim_lua = '[Lua]',
+        --     cmdline = '[CMD]',
+        --   },
+        -- },
+        fields = { 'kind', 'abbr', 'menu' },
+        format = function(entry, vim_item)
+          local kind = require('lspkind').cmp_format { mode = 'symbol_text', maxwidth = 50 }(entry, vim_item)
+          local strings = vim.split(kind.kind, '%s', { trimempty = true })
+          kind.kind = ' ' .. (strings[1] or '') .. ' '
+          kind.menu = '    (' .. (strings[2] or '') .. ')'
+
+          return kind
+          -- local lspkind_ok, lspkind = pcall(require, 'lspkind')
+          -- if not lspkind_ok then
+          --   -- 设置图标
+          --   vim_item.kind = string.format('%s %s', icons.kinds[vim_item.kind], vim_item.kind)
+          --   vim_item.menu = ({
+          --     buffer = '[Buffer]',
+          --     nvim_lsp = '[LSP]',
+          --     path = '[Path]',
+          --     nvim_lua = '[Lua]',
+          --     cmdline = '[CMD]',
+          --     luasnip = "[LuaSnip]",
+          --   })[entry.source.name]
+          --   return vim_item
+          -- else
+          --   return lspkind.cmp_format()(entry, vim_item)
+          -- end
+        end,
       },
       experimental = {
         ghost_text = {
@@ -66,8 +148,8 @@ return {
         ['<Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          -- elseif luasnip.expand_or_jumpable() then
-          --   luasnip.expand_or_jump()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
           elseif has_words_before() then
             cmp.complete()
           else
@@ -77,8 +159,8 @@ return {
         ['<S-Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          -- elseif luasnip.jumpable(-1) then
-          --   luasnip.jump(-1)
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
           else
             fallback()
           end
@@ -98,6 +180,15 @@ return {
       sources = {
         { name = 'buffer' },
       },
+    })
+    cmp.setup.filetype('lua',{
+      sources = cmp.config.sources({
+        {name = 'luasnip'}
+      },{
+        {name = 'nvim_lua'},
+        {name = 'buffer'},
+        {name = 'path'}
+      })
     })
     -- cmp.setup.cmdline(':', {
     --   mapping = cmp.mapping.preset.cmdline(),
