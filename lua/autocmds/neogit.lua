949d03c5fd185ce85b6cebad318e938f8f5002e6
somnus9527 <somnus9527@yeah.net>
2024-02-04 11:06:21 +0800

    取消neogit的autocmd，经常报错

--- lua/autocmds/neogit.lua
+++ lua/autocmds/neogit.lua
@@ -1,9 +1,9 @@
-local group = vim.api.nvim_create_augroup('SomnusNeogit', { clear = true })
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'NeogitPushComplete',
-  group = group,
-  -- callback = require('neogit').close,
-  callback = function()
-      vim.cmd('q')
-  end
-})
+-- local group = vim.api.nvim_create_augroup('SomnusNeogit', { clear = true })
+-- vim.api.nvim_create_autocmd('User', {
+--   pattern = 'NeogitPushComplete',
+--   group = group,
+--   -- callback = require('neogit').close,
+--   callback = function()
+--       vim.cmd('q')
+--   end
+-- })
