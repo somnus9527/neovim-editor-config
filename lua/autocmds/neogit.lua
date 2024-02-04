190ca995050e64b39d1bd02c77fd6b7f42fecea5
somnus9527 <somnus9527@yeah.net>
2024-02-04 11:02:12 +0800

    修改neogit push之后的callback

--- lua/autocmds/neogit.lua
+++ lua/autocmds/neogit.lua
@@ -2,5 +2,8 @@ local group = vim.api.nvim_create_augroup('SomnusNeogit', { clear = true })
 vim.api.nvim_create_autocmd('User', {
   pattern = 'NeogitPushComplete',
   group = group,
-  callback = require('neogit').close,
+  -- callback = require('neogit').close,
+  callback = function()
+      vim.cmd('bdelete')
+  end
 })
