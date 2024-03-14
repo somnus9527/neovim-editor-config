13a2bde55214cdd5bb9afe164e54a3d0e55b092e
somnus9527 <somnus9527@yeah.net>
2024-03-14 10:40:59 +0800

    1. 增加LoadSession自定义命令用来手动加载当前工作目录的session 2.    优化mini.surround的配置

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -3,11 +3,11 @@ return {
   event = { 'BufNewFile', 'BufReadPre' },
   opts = {
     mappings = {
-      add = 'ys',
-      delete = 'ds',
-      find = 'gsf',
-      highlight = 'gsh',
-      replace = 'cs',
+      add = 'sa',
+      delete = 'sd',
+      -- find = 'gsf',
+      -- highlight = 'gsh',
+      -- replace = 'cs',
       update_n_lines = 'gsn',
       find = 'sf', -- Find surrounding (to the right)
       find_left = 'sF', -- Find surrounding (to the left)
