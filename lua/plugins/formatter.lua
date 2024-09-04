8c211b99b094073827dc8a4cb1420128ae9e300f
wangxp <wangxpb@digiwin.com>
2024-09-04 09:45:08 +0800

    feat: 除了格式化的缺省配置

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,46 +1,64 @@
 return {
-  "mhartington/formatter.nvim",
-  lazy = true,
-  module = true,
-  config = function ()
-    local tools = require "tools.tools"
-    local formatter = require("formatter")
-    local web_format = function ()
-      local is_eslint_project = tools.is_eslint_project();
-      if is_eslint_project then
-        return {
-          exe = "eslint_d",
-          args = { "--stdin", "--stdin-filename", vim.api.nvim_buf_get_name(0), "--fix-to-stdout" },
-          stdin = true,
-        }
-      else
-        return {
-          exe = "tsserver",
-          args = { "--stdin", "--stdin-filename", vim.api.nvim_buf_get_name(0) },
-          stdin = true,
-        }
-      end
-    end
-    local opt = {
-      logging = false,
-      filetype = {
-        typescriptreact = {
-          web_format,
-        },
-        typescript = {
-          web_format,
-        },
-        javascript = {
-          web_format,
-        },
-        javascriptreact = {
-          web_format,
-        },
-        lua = {
-          require("formatter.filetypes.lua").stylua,
-        }
-      }
-    }
-    formatter.setup(opt)
-  end
+	"mhartington/formatter.nvim",
+	lazy = true,
+	module = true,
+	config = function()
+		local tools = require("tools.tools")
+		local formatter = require("formatter")
+		local util = require("formatter.util")
+		local web_format = function()
+			local is_eslint_project = tools.is_eslint_project()
+			local is_prettier_project = tools.is_prettier_project()
+			if is_eslint_project then
+				return {
+					exe = "eslint_d",
+					args = { "--stdin", "--stdin-filename", vim.api.nvim_buf_get_name(0), "--fix-to-stdout" },
+					stdin = true,
+				}
+			elseif is_prettier_project then
+				return {
+					exe = "prettierd",
+					args = { util.escape_path(util.get_current_buffer_file_path()) },
+					stdin = true,
+				}
+			else
+				return {
+					exe = "tsfmt",
+					args = {
+						"--stdin",
+						"--baseDir",
+						util.escape_path(util.get_cwd()),
+					},
+					stdin = true,
+					try_node_modules = true,
+				}
+			end
+		end
+		local opt = {
+			logging = true,
+			filetype = {
+				typescriptreact = {
+					web_format,
+				},
+				typescript = {
+					web_format,
+				},
+				javascript = {
+					web_format,
+				},
+				javascriptreact = {
+					web_format,
+				},
+				lua = {
+					require("formatter.filetypes.lua").stylua,
+				},
+				["*"] = {
+					-- "formatter.filetypes.any" defines default configurations for any
+					-- filetype
+					require("formatter.filetypes.any").remove_trailing_whitespace,
+				},
+			},
+		}
+		formatter.setup(opt)
+	end,
 }
