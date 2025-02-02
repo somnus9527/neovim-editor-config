c8620a3190fd18631eb8bc8df8892be9b55a73c4
wangxp <wangxpb@digiwin.com>
2025-02-02 15:43:11 +0800

    feat: Snack配置更新

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      win = {
+        input = {
+          keys = {
+            ["<A-n>"] = { "list_down", mode = { "i", "n" } },
+            ["<A-m>"] = { "list_up", mode = { "i", "n" } },
+            ["<A-e>"] = { "close", mode = { "i", "n" } },
+            ["<A-,>"] = { "list_scroll_down", mode = { "i", "n" } },
+            ["<A-.>"] = { "list_scroll_up", mode = { "i", "n" } },
+            ["<A-k>"] = { "preview_scroll_up", mode = { "i", "n" } },
+            ["<A-j>"] = { "preview_scroll_down", mode = { "i", "n" } },
+          },
+        },
+      },
+    },
+  },
+}
