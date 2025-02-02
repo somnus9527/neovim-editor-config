5160135b531482d0b0197f970e425a54d0503141
wangxp <wangxpb@digiwin.com>
2025-02-02 15:07:09 +0800

    feat: 更新配置

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,7 +1,10 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    close_if_last_window = true,
     window = {
+      position = "left",
+      width = 80,
       mappings = {
         ["s"] = "none",
         ["S"] = "none",
@@ -9,6 +12,13 @@ return {
       fuzzy_finder_mappings = {
         ["<A-n>"] = "move_cursor_down",
         ["<A-m>"] = "move_cursor_up",
+      }
+    },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
       }
     }
   }
