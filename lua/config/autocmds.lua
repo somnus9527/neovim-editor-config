64d365f34d0fb0e4fdfbbe2091d15d45dafbe5f8
wangxp <wangxpb@digiwin.com>
2025-04-03 16:40:32 +0800

    feat: 优化toggleterm快捷键配置

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -168,7 +168,10 @@ vim.api.nvim_create_autocmd("FileType", {
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
   vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-  vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  -- 仅在 toggleterm 里绑定 `jk`
+  if vim.bo.filetype == "toggleterm" then
+    vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  end
   vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
   vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
