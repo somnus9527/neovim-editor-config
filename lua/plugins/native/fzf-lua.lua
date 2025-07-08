2e4db5ce63afb8bb8134c87cd646be7dd74eee4c
wangxp <wangxpb@digiwin.com>
2025-07-08 13:41:55 +0800

    feat: 增加bookmark配置

--- lua/plugins/native/fzf-lua.lua
+++ lua/plugins/native/fzf-lua.lua
@@ -199,7 +199,7 @@ return {
     },
     { "<leader>li", "<cmd>lua require('fzf-lua').lsp_implementations()<CR>", desc = "FZF搜索implementations" },
     { "<leader>`", LazyVim.pick("colorschemes"), desc = "主题切换" },
-    { "<leader>m", LazyVim.pick("marks"), desc = "mark列表" },
+    -- { "<leader>m", LazyVim.pick("marks"), desc = "mark列表" },
     {
       "<leader>ll",
       function()
