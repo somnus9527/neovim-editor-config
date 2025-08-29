796148c9d37af286e81f34f201cd6d51fb7d62ad
wangxp <wangxpb@digiwin.com>
2025-08-29 11:28:00 +0800

    feat: 配置更新

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,24 +1,50 @@
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    lazy = true,
-    name = "gruvbox"
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "moon" },
-  },
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      integrations = { blink_cmp = true },
-    },
-    config = function()
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = true,
+		name = "gruvbox",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme("gruvbox")
+    -- end
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		opts = { style = "moon" },
+	},
+	{
+		"rose-pine/neovim",
+		lazy = true,
+		-- lazy = false,
+		-- priority = 1000,
+		name = "rose-pine",
+		-- config = function()
+		-- 	vim.cmd.colorscheme("rose-pine")
+		-- end,
+	},
+	{
+		"zenbones-theme/zenbones.nvim",
+		dependencies = "rktjmp/lush.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.zenbones_darken_comments = 45
+			vim.cmd.colorscheme("zenbones")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		lazy = true,
+		-- lazy = false,
+		-- priority = 1000,
+		name = "catppuccin",
+		opts = {
+			integrations = { blink_cmp = true },
+		},
+		-- config = function()
+		-- 	vim.cmd.colorscheme("catppuccin")
+		-- end,
+	},
 }
