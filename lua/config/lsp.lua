77eaeba76f3c394c9aac60120e760952a24249e9
wangxp <wangxpb@digiwin.com>
2025-09-15 09:04:58 +0800

    feat: 配置更新

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -36,11 +36,32 @@ local servers = {
 		filetypes = {
 			"javascript",
 			"javascriptreact",
-			"javascript.jsx",
 			"typescript",
 			"typescriptreact",
-			"typescript.tsx",
 		},
+		root_dir = function(bufnr, on_dir)
+			-- The project root is where the LSP can be started from
+			-- As stated in the documentation above, this LSP supports monorepos and simple projects.
+			-- We select then from the project root, which is identified by the presence of a package
+			-- manager lock file.
+			local root_markers = {
+				"package-lock.json",
+				"yarn.lock",
+				"pnpm-lock.yaml",
+				"bun.lockb",
+				"bun.lock",
+				"deno.lock",
+				"package.json",
+				"pnpm-workspace.yaml",
+			}
+			-- Give the root markers equal priority by wrapping them in a table
+			root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers, { ".git" } }
+				or vim.list_extend(root_markers, { ".git" })
+			-- We fallback to the current working directory if no project root is found
+			local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+
+			on_dir(project_root)
+		end,
 		settings = {
 			complete_function_calls = true,
 			vtsls = {
@@ -69,14 +90,14 @@ local servers = {
 			},
 		},
 	},
-  bashls = {},
-  css_variables = {},
-  cssmodules_ls = {},
-  html = {},
-  tailwindcss = {},
-  vue_ls = {},
-  jsonls = {},
-  -- svelte = {},
+	bashls = {},
+	css_variables = {},
+	cssmodules_ls = {},
+	html = {},
+	tailwindcss = {},
+	vue_ls = {},
+	jsonls = {},
+	-- svelte = {},
 }
 
 for server, config in pairs(servers) do
