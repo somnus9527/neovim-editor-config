6537e640a943c83579971425d8370ee83ba407e7
wangxp <wangxpb@digiwin.com>
2025-11-24 16:13:09 +0800

    feat: 移除timespent

--- lua/plugins/timespent.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"quentingruber/timespent.nvim",
-  cmd = "ShowTimeSpent",
-	keys = {
-		-- { "<leader>ts", "<cmd>:ShowTimeSpent<cr>", mode = { "n" }, desc = "Show time spent" },
-		-- { "<leader>ts", "<cmd>:ExportTimeSpent<cr>", mode = { "n" }, desc = "export time spent" },
-	},
-}
