a7a021eeb7f21cb47e873e9a1585b407f6c252b3
wangxp <wangxpb@digiwin.com>
2026-01-08 13:27:40 +0800

    feat: 移除luasnip历史，经常会造成意外的操作出现

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -12,6 +12,9 @@ return {
 			"L3MON4D3/LuaSnip",
 			version = "v2.*",
 			build = "make install_jsregexp",
+			opts = {
+				history = false,
+			},
 			config = function()
 				require("luasnip.loaders.from_lua").lazy_load({
 					paths = { vim.fn.stdpath("config") .. "/lua/snippets" },
